f492538c2864737e8dd6933d026d5e99cb43b2e9
Puneet Tomar <tomarpuneet79@gmail.com>
2025-06-13 13:53:45 +0530

    changes done in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}", // if you're using the app directory
-  ],
-  theme: {
+ content: [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./app/**/*.{js,ts,jsx,tsx}", // if you're using app directory
+],
+theme: {
     extend: {},
   },
   plugins: [],
