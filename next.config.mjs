c4a76161fdabd567f8d7043a905ae56c017f09d0
Puneet Tomar <tomarpuneet79@gmail.com>
2025-06-13 14:11:39 +0530

    changes done in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Enables static export (for `next export`)
   images: {
     domains: ['www.dituniversity.edu.in'], // For loading external images
     unoptimized: true, // ✅ Disable image optimization for static export
